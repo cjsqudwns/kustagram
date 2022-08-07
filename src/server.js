5a37f4e56e054bccb1d722ae9b35ce14c06d1e93
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-08-07 14:57:39 +0900

    Connect to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
+import "dotenv/config";
+import "./db";
 import express from "express";
 import path from "path";
 import morgan from "morgan";
+import User from "./models/User";
 
 const app = express();
 const logger = morgan("dev");
+const PORT = 4000;
+const handleListening = () =>
+  console.log(`✅ Listening on PORT http://localhost:${PORT}`);
 
 app.use(logger);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
-const PORT = 4000;
-const handleListening = () =>
-  console.log(`Listening on PORT http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
